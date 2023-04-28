594a5b6e50a1f217e517f5f34d3470e3f7e082ed
Ryan Tan <ryantan.mail@gmail.com>
2023-04-27 20:54:12 -0600

    start of a snake project

---
+++ Snake/snake.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARENA_WIDTH 50
+#define ARENA_HEIGHT 50
+
+void prompt(){
+	int c;
+	printf("Enter a name: \n");
+	while(EOF != (c = getchar())){
+		putchar(c);	
+	}
+}
+
+void drawArena(){
+	for (int i = 0; i < ARENA_HEIGHT; i++){
+		printf("|");
+		for (int j = 1; j < ARENA_WIDTH; j++){
+			if (i == 0) {
+				printf("-");	
+			} else if(i == ARENA_HEIGHT - 1) {
+				printf("_");	
+			} else {
+				printf(" ");
+			}
+		}
+		printf("|\n");
+	}	
+}
+
+//Driver function
+int main (){
+	drawArena();
+	return 0;
+}
